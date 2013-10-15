6c708d1fe5f5a5f5c5d82a6dc61be1ee26b94ac6
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-10-15 12:33:11 -0700

    Server serves HTML and the libraries but not the script files yet

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,14 +1,6 @@
 /* Import node's http module: */
 var http = require("http");
 var handle = require('./request-handler');
-
-var requestListener = function (request, response) {
-  console.log("Serving request type " + request.method + " for url " + request.url);
-  var text = handle.handleRequest(request, response);
-  console.log("THIS IS THE TEXT");
-  console.log(text);  
-};
-
 var port = 8080;
 var ip = "127.0.0.1";
 
